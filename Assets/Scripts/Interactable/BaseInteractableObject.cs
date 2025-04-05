fb9ff9cfb261bf143d1f06e1fe9d5644bd99a9eb
Erosrolf <Erosrolf@gmail.com>
2025-04-05 18:56:55 +0300

    added interactable base class, and crosshair reycaster

---
+++ Assets/Scripts/Interactable/BaseInteractableObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace MrLucy
+{
+    [RequireComponent(typeof(Collider))]
+    public abstract class BaseInteractableObject : MonoBehaviour
+    {
+        private const int InteractableLayer = 6;
+
+        protected virtual void Awake()
+        {
+            if (gameObject.layer != InteractableLayer)
+            {
+                gameObject.layer = InteractableLayer;
+#if UNITY_EDITOR
+                Debug.LogWarning(
+                    $"{gameObject.name}: Автоматически установлен слой Interactable (слой {InteractableLayer})");
+#endif
+            }
+        }
+
+        public abstract void Interact();
+    }
+}
---
+++ Assets/Scripts/Interactable/ElevatorButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace MrLucy
+{
+    public class ElevatorButton : BaseInteractableObject
+    {
+        public int buttonNumber;
+        
+        public override void Interact()
+        {
+            Debug.Log($"Interacting with ElevatorButton {buttonNumber}");
+        }
+    }
+}
