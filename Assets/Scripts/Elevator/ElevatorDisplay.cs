561b994f2495774782799a4a8a6fc01b0441b614
Erosrolf <Erosrolf@gmail.com>
2025-04-06 12:13:35 +0300

    Add ElevatorLight, ElevatorDisplay and DownhillScenario

---
+++ Assets/Scripts/Elevator/ElevatorDisplay.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace MrLucy
+{
+    public class ElevatorDisplay : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI elevatorDisplayText;
+
+        private int _floorNumber;
+
+        public int FloorNumber
+        {
+            get => _floorNumber;
+            set
+            {
+                _floorNumber = value;
+                elevatorDisplayText.text = value.ToString();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Elevator/ElevatorLight.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace MrLucy
+{
+    public class ElevatorLight : MonoBehaviour
+    {
+        [SerializeField] private Light elevatorLight; // обычный Unity Light
+        [SerializeField] private Renderer[] lightedSurfaces; // объекты, чьи материалы имитируют освещение
+        [SerializeField] private Color normalColor = Color.white;
+        [SerializeField] private Color warningColor = Color.red;
+
+        private Coroutine _blinkingRoutine;
+
+        public void TurnOn()
+        {
+            SetLightActive(true);
+        }
+
+        public void TurnOff()
+        {
+            SetLightActive(false);
+        }
+
+        public void SetWarningMode(bool enabled)
+        {
+            SetLightColor(enabled ? warningColor : normalColor);
+        }
+
+        public void StartBlinking(float interval = 0.3f)
+        {
+            if (_blinkingRoutine == null)
+                _blinkingRoutine = StartCoroutine(BlinkRoutine(interval));
+        }
+
+        public void StopBlinking()
+        {
+            if (_blinkingRoutine != null)
+            {
+                StopCoroutine(_blinkingRoutine);
+                _blinkingRoutine = null;
+                SetLightActive(true); // вернём свет обратно
+            }
+        }
+
+        private void SetLightActive(bool isOn)
+        {
+            if (elevatorLight != null)
+                elevatorLight.enabled = isOn;
+
+            foreach (var r in lightedSurfaces)
+            {
+                r.material.EnableKeyword("_EMISSION");
+                r.material.SetColor("_EmissionColor", isOn ? normalColor * 1.5f : Color.black);
+            }
+        }
+
+        private void SetLightColor(Color color)
+        {
+            if (elevatorLight != null)
+                elevatorLight.color = color;
+
+            foreach (var r in lightedSurfaces)
+            {
+                r.material.SetColor("_EmissionColor", color * 1.5f);
+            }
+        }
+
+        private IEnumerator BlinkRoutine(float interval)
+        {
+            bool state = true;
+            while (true)
+            {
+                SetLightActive(state);
+                state = !state;
+                yield return new WaitForSeconds(interval);
+            }
+        }
+    }
+}
