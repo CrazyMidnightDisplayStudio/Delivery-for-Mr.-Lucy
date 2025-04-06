957a770a1943f7ab77a41be260da3ea8cf41c671
Erosrolf <Erosrolf@gmail.com>
2025-04-06 18:47:17 +0300

    add elevatorButton sound

---
+++ Assets/Scripts/AudioMixer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+namespace MrLucy
+{
+    public class AudioManager : MonoBehaviour
+    {
+        public static AudioManager Instance { get; private set; }
+
+        public AudioMixer audioMixer;
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+
+            if (audioMixer == null)
+            {
+                audioMixer = Resources.Load<AudioMixer>("Audio/AudioMixer");
+            }
+
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Interactable/ElevatorButton.cs
+++ Assets/Scripts/Interactable/ElevatorButton.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Audio;
 
 namespace MrLucy
 {
@@ -6,16 +7,25 @@ namespace MrLucy
     {
         public int buttonNumber;
         private ButtonPressAnimation _pressAnimation;
+        
+        private AudioSource _audioSource;
+        private AudioClip _buttonPressSound;
 
         protected override void Awake()
         {
             base.Awake();
             _pressAnimation = gameObject.AddComponent<ButtonPressAnimation>();
+            _audioSource = gameObject.AddComponent<AudioSource>();
+            _audioSource.playOnAwake = false;
+            _buttonPressSound = Resources.Load<AudioClip>("Audio/Sounds/elevatorButton");
+            
+            _audioSource.outputAudioMixerGroup = AudioManager.Instance.audioMixer.FindMatchingGroups("Sounds")[0];
         }
         
         public override void Interact()
         {
             _pressAnimation.PushButton();
+            _audioSource.PlayOneShot(_buttonPressSound);
             if (!isInteractActive) return;
             Debug.Log($"Interacting with ElevatorButton {buttonNumber}");
         }
