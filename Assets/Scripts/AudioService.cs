a5ff356ea52bbb1843cac7048e3ecdfa457855ab
Erosrolf <Erosrolf@gmail.com>
2025-04-07 23:17:39 +0300

    AudioService.cs

---
+++ Assets/Scripts/AudioService.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+namespace MrLucy
+{
+    public class AudioService : Singleton<AudioService>
+    {
+        [SerializeField] private AudioMixer mixer;
+
+        private AudioMixerGroup _masterGroup;
+        private int _volume = 60;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            _masterGroup = mixer.FindMatchingGroups("Master")[0];
+            DontDestroyOnLoad(this);
+            SetMasterVolume(_volume);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Minus))
+            {
+                _volume = Mathf.Clamp(_volume - 5, 0, 100);
+                SetMasterVolume(_volume);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Equals) || Input.GetKeyDown(KeyCode.Plus))
+            {
+                _volume = Mathf.Clamp(_volume + 5, 0, 100);
+                SetMasterVolume(_volume);
+            }
+        }
+
+        public void SetMasterVolume(int volumePercent)
+        {
+            _volume = Mathf.Clamp(volumePercent, 0, 100);
+
+            float dB = Mathf.Lerp(-80f, 0f, _volume / 100f);
+            _masterGroup.audioMixer.SetFloat("MasterVolume", dB);
+        }
+    }
+}
